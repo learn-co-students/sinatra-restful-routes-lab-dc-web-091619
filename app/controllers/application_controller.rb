75e20eb5f9d091cb76b108bc9a4edead6632d925
Will <comfortwb21@gmail.com>
2019-10-08 17:37:36 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,51 @@
+require "pry"
+
 class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
 
-  # code actions here!
+  get '/recipes' do
+    @recipes = Recipe.all 
+    erb :index
+  end
+
+  post '/recipes' do 
+    name = params[:name]
+    ingredients = params[:ingredients]
+    cook_time = params[:cook_time]
+    @recipe = Recipe.create(name: name, ingredients: ingredients, cook_time: cook_time)
+    redirect "/recipes/#{@recipe.id}"
+  end
+
+  get '/recipes/new' do 
+    erb :new 
+  end
+
+  get '/recipes/:id' do
+    id = params[:id]
+    @recipe = Recipe.find(params[:id])
+    erb :show
+  end
+
+  get '/recipes/:id/edit' do 
+    @recipe = Recipe.find(params[:id])
+    erb :edit
+  end
+
+  patch '/recipes/:id' do 
+    @recipe = Recipe.find(params[:id])
+    @recipe.update(name: params[:name],
+                  ingredients: params[:ingredients,
+                  cook_time: params[:cook_time]])
+    redirect "/recipes/#{@recipe.id}"
+  end
+
+  delete '/recipes/:id' do 
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+    redirect '/recipes'
+  end
 
 end
